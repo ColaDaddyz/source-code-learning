a862dc8efd84fcd8729cde04c1df32d288cc8da1
ZhangFe <zhangzhe0214@gmail.com>
2017-07-19 08:50:24 +0800

    更新 格式化代码

--- redux/applyMiddleware.js
+++ redux/applyMiddleware.js
@@ -17,21 +17,21 @@ import compose from './compose'
  * @returns {Function} A store enhancer applying the middleware.
  */
 export default function applyMiddleware(...middlewares) {
-  return (createStore) => (reducer, preloadedState, enhancer) => {
-    var store = createStore(reducer, preloadedState, enhancer)
-    var dispatch = store.dispatch
-    var chain = []
+    return (createStore) => (reducer, preloadedState, enhancer) => {
+        const store = createStore(reducer, preloadedState, enhancer)
+        let dispatch = store.dispatch
+        let chain = []
 
-    var middlewareAPI = {
-      getState: store.getState,
-      dispatch: (action) => dispatch(action)
-    }
-    chain = middlewares.map(middleware => middleware(middlewareAPI))
-    dispatch = compose(...chain)(store.dispatch)
+        const middlewareAPI = {
+            getState: store.getState,
+            dispatch: (action) => dispatch(action)
+        }
+        chain = middlewares.map(middleware => middleware(middlewareAPI))
+        dispatch = compose(...chain)(store.dispatch)
 
-    return {
-      ...store,
-      dispatch
+        return {
+            ...store,
+            dispatch
+        }
     }
-  }
 }
--- redux/compose.js
+++ redux/compose.js
@@ -9,15 +9,13 @@
  */
 
 export default function compose(...funcs) {
-  if (funcs.length === 0) {
-    return arg => arg
-  }
+    if (funcs.length === 0) {
+        return arg => arg
+    }
 
-  if (funcs.length === 1) {
-    return funcs[0]
-  }
+    if (funcs.length === 1) {
+        return funcs[0]
+    }
 
-  const last = funcs[funcs.length - 1]
-  const rest = funcs.slice(0, -1)
-  return (...args) => rest.reduceRight((composed, f) => f(composed), last(...args))
+    return funcs.reduce((a, b) => (...args) => a(b(...args)))
 }
